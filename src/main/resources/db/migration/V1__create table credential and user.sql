a4250159f3e8cedbcfbb9b029e20c7dc6758f3d3
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2023-10-29 17:01:37 -0300

    `feat:` Initializing the development of the migrations

---
+++ src/main/resources/db/migration/V1__create table credential and user.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS credential (
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    expired BOOLEAN NOT NULL DEFAULT false,
+    userId bigint NOT NULL
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS user (
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    credentialId bigint NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (credentialId) REFERENCES credential (id)
+);
+
+ALTER TABLE credential ADD CONSTRAINT fk_user FOREIGN KEY (user) REFERENCES user (id);
