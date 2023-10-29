7d519a827fa51f2f9c2c9f2524e20834d39ccb27
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2023-10-29 16:23:57 -0300

    `feat:` Initial commit

---
+++ src/main/java/br/com/api/shoppingtool/ShoppingToolApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.shoppingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingToolApplication.class, args);
+	}
+
+}
